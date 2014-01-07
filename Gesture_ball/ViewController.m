50a8f70dabceb2b8f3ea19f5e4b831b35f126773
steve0503 <leehr76@gmail.com>
2014-01-07 23:30:25 +0900

    Gesture_4_2_2
    Done

--- Gesture_ball/ViewController.m
+++ Gesture_ball/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  Gesture_ball
+//  Gesture_4_2_2
 //
 //  Created by 이 해용 on 2014. 1. 7..
 //  Copyright (c) 2014년 iamdreamer. All rights reserved.
@@ -10,14 +10,62 @@
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UIImageView *ballImage;
+
+
 @end
 
 @implementation ViewController
+
+- (IBAction)handleRotate:(UIRotationGestureRecognizer *)gesture {
+    
+    self.ballImage.transform = CGAffineTransformRotate(self.ballImage.transform, gesture.rotation);
+    
+    [gesture setRotation:0.0];
+    
+}
+
+
+
+-(void)handlePan:(UIPanGestureRecognizer *)gesture{
+    
+    CGPoint point = [gesture locationInView:self.view];
+    
+    self.ballImage.center = point;
+    
+    
+}
+
+-(void)handlePinch:(UIPinchGestureRecognizer *)gesture{
+    
+    CGFloat scale = gesture.scale;
+    
+    self.ballImage.transform = CGAffineTransformScale(self.ballImage.transform, scale, scale);
+    
+    [gesture setScale:1.0];
+    
+    
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    
+    [self.ballImage addGestureRecognizer:pan];
+    
+    
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+    
+    [self.ballImage addGestureRecognizer:pinch];
+    
+    UIRotationGestureRecognizer *rotate = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotate:)];
+    
+    [self.ballImage addGestureRecognizer:rotate];
+    
 }
 
 - (void)didReceiveMemoryWarning
